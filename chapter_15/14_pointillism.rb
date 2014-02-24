af6c4445eaaacf0be95bedf1b43d4221ff99a9e9
monkstone <martin_p@lineone.net>
2014-02-24 18:57:59 +0000

    Add autorun Rakefile

--- chapter_15/14_pointillism.rb
+++ chapter_15/14_pointillism.rb
@@ -1,34 +1,29 @@
-require 'ruby-processing'
 
-class PointillismSketch < Processing::App
-
-  def setup
-    @pointillize = 10
-    @img = load_image 'sunflower.jpg'
-    background 255
-    frame_rate 200 # Let's run this one as fast as we can.
-    no_stroke
-    smooth
-  end
-
-  def draw
-    # Pick a random point
-    x, y = rand(width), rand(height)
-    loc = x + y * width
-
-    # Look up the RGB color in the source image
-    load_pixels
-    pixel = @img.pixels[loc]
-    r, g, b = red(pixel), green(pixel), blue(pixel)
-    
-    # Draw an ellipse at that location with that color
-    fill r, g, b, 100
-    # Back to shapes! Instead of setting a pixel, we use the color from a pixel to draw a circle.
-    ellipse x, y, @pointillize, @pointillize
-  end
-
+def setup
+  size 200, 200
+  @pointillize = 10
+  @img = load_image 'sunflower.jpg'
+  background 255
+  frame_rate 200 # Let's run this one as fast as we can.
+  no_stroke
 end
 
-PointillismSketch.new :title => "Pointillism", :width => 200, :height => 200
+def draw
+  # Pick a random point
+  x, y = rand(width), rand(height)
+  loc = x + y * width
+
+  # Look up the RGB color in the source image
+  load_pixels
+  pixel = @img.pixels[loc]
+  r, g, b = red(pixel), green(pixel), blue(pixel)
+  
+  # Draw an ellipse at that location with that color
+  fill r, g, b, 100
+  # Back to shapes! Instead of setting a pixel, we use the color from a pixel to draw a circle.
+  ellipse x, y, @pointillize, @pointillize
+end
+
+
 
 
